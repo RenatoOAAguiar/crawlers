ea98ab5dd5aa3344011127fea6cce10e3d5e8e15
Renato Aguiar <renatooaa@outlook.com>
2018-08-29 16:54:54 -0300

    correções

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Crawller de dados
+
+## dependency to install textract
+
+* apt instlal libpulse-dev
+* apt install swig
--- extractFromPdf.py
+++ extractFromPdf.py
@@ -1,9 +1,8 @@
 import textract
 from os import walk
 
-
-
-f = []
 for (dirpath, dirnames, filenames) in walk("pdf/"):
-    text = textract.process("path/to/file.extension")
-    print(text)
+    for arquivo in filenames:
+        text = textract.process(dirpath + arquivo)
+        with open('contentBooks/' + arquivo.split(".pdf")[0] + '.txt', 'a', encoding="uft-8") as f:
+            f.write(text.decode("utf-8"))
